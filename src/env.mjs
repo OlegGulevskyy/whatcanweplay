599d2ac8a01ecb22d5947e635bfcf2b97a5cdcd4
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2024-04-02 17:30:44 +0200

    posthog

--- src/env.mjs
+++ src/env.mjs
@@ -30,7 +30,8 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: z.string().optional(),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
   },
 
   /**
@@ -45,7 +46,8 @@ export const env = createEnv({
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
-    NEXT_PUBLIC_UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,20 +1,17 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import {
-  AnalyticsProvider,
-  UmamiAnalyticsProvider,
-} from "./AnalyticsProvider/AnalyticsProvider";
 import React, { type PropsWithChildren } from "react";
+import { CSPostHogProvider } from "./PosthogProvider";
 
 export function Providers({ children }: PropsWithChildren) {
   return (
     <>
-      <ThemeProvider attribute="class" forcedTheme="light">
-        {children}
-      </ThemeProvider>
-      <AnalyticsProvider />
-      <UmamiAnalyticsProvider />
+      <CSPostHogProvider>
+        <ThemeProvider attribute="class" forcedTheme="light">
+          {children}
+        </ThemeProvider>
+      </CSPostHogProvider>
     </>
   );
 }
